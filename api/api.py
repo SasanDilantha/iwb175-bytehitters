496affcc1d6c4a8dc748d40e1dc77886d6c66d4e
@SIA <59891156+SA-Tester@users.noreply.github.com>
2024-10-19 20:18:45 +0530

    update api.py to return shortage date

--- api/api.py
+++ api/api.py
@@ -1,4 +1,5 @@
 import init_daily_power
+import init_shortage_prediction
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -9,3 +10,11 @@ def daily_power_allocation():
     total_power_available = data['total_power_available'] ## UPDATE FROM DATABASE
     allocations = init_daily_power.get_daily_power_prediction(total_power_available)
     return jsonify({'allocations': allocations})
+
+
+@app.route('/prediction_date', methods=['POST'])
+def prediction_date():
+    next_date_of_shortage = init_shortage_prediction.get_next_date_of_shortage()
+    return jsonify({'prediction_date': next_date_of_shortage})
+
+# shortage_amount
